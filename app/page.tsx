5cc01ed3d3aea013dc98fa0192bb4289b2173fcf
Vova <bordiian.v@gmail.com>
2023-04-12 10:55:49 +0300

    added simple Navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className='px-6 mx-auto'>
-      <p className='mt-12 mb-12 text-3xl text-center dark:text-white'>
+      <p className='mt-12 mb-12 text-3xl text-center text-white'>
         Hello and Welcome! &nbsp;
         <span className='whitespace-nowrap'>
           I'm <span className='font-bold'>Vova</span>
