9e242bd906357d4385e07a2eed0038892d1c9cac
Vova <bordiian.v@gmail.com>
2023-04-24 11:40:10 +0300

    dynamicaly generate html data to post page now

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,5 +1,7 @@
-import { getSortedPostsData } from "@/lib/posts"
+import { getSortedPostsData, getPostData } from "@/lib/posts"
 import { notFound } from 'next/navigation'
+import getFormattedDate from "@/lib/getFormattedDate"
+import Link from "next/link"
 
 export function generateMetadata({ params }: { params: { postId: string }}) {
 
@@ -26,8 +28,23 @@ async function Post({ params }: { params: { postId: string }}) {
     return notFound()
   }
 
+  const  { title, date, contentHtml } = await getPostData(postId)
+
+  const pubDate = getFormattedDate(date)
+
   return (
-    <div>page</div>
+    <main className="px-6 prose prose-xl prose-slate mx-auto">
+      <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+      <p className="mt-0">
+        {pubDate}
+      </p>
+      <article>
+        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        <p>
+          <Link href='/'>Back to Home</Link>
+        </p>
+      </article>
+    </main>
   )
 }
 
